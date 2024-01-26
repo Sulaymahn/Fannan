daf855b6fd06c6e9ca74e4592c5c1099959313b1
Sulaymahn Mukhtar <sulmuk28@gmail.com>
2024-01-26 23:05:19 +0100

    Add project files.

---
+++ Fannan.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fannan.API.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class AuthenticationController
+    {
+        [HttpGet("auth")]
+        public string Authenticate()
+        {
+            return "Authenticated";        }
+    }
+}
